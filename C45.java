9067e0ca0c79526fc9fc6f72f77505fc5bf6c28c
Cleverson Nahum <cleversonahum@gmail.com>
2018-05-14 10:01:10 -0300

    Tree visualization added

--- C45.java
+++ C45.java
@@ -3,9 +3,13 @@ import weka.filters.Filter;
 import weka.classifiers.trees.J48;
 import weka.filters.unsupervised.attribute.Remove;
 import weka.classifiers.Evaluation;
+import weka.gui.treevisualizer.PlaceNode2;
+import weka.gui.treevisualizer.TreeVisualizer;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Random;
+import java.awt.BorderLayout;
 
 class C45 {
 
@@ -32,12 +36,33 @@ class C45 {
 		J48 tree = new J48();         // new instance of tree
 		tree.setOptions(options);     // set the options
 		tree.buildClassifier(data);   // build classifier
+		System.out.println("Graph:\n\n"+tree.graph()+"\n\n\n\n");
+		//System.out.println("Algorithm described in the Tree:\n\n"+tree.toSource()+"\n\n\n\n");
 
 		//Evaluation
 		//Using Cross-Validation
 		Evaluation eval = new Evaluation(data);
 		eval.crossValidateModel(tree, data, 10, new Random(1));
 		System.out.println(eval.toSummaryString("\nResults\n======\n", false));
+
+		//Data Visualization
+		// display classifier
+		final javax.swing.JFrame jf = 
+			new javax.swing.JFrame("Weka Classifier Tree Visualizer: J48");
+		jf.setSize(2000,2000);
+		jf.getContentPane().setLayout(new BorderLayout());
+		TreeVisualizer tv = new TreeVisualizer(null,
+				tree.graph(),
+				new PlaceNode2());
+		jf.getContentPane().add(tv, BorderLayout.CENTER);
+		jf.addWindowListener(new java.awt.event.WindowAdapter() {
+			public void windowClosing(java.awt.event.WindowEvent e) {
+				jf.dispose();
+			}
+		});
+
+		jf.setVisible(true);
+		tv.fitToScreen();
 	}
 }
 
